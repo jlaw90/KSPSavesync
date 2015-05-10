900170fe265c2862441b1573a7b3a1e112156614
James Lawrence <jlaw90@hotmail.com>
2015-05-10 20:05:48 +0100

    Change AssemblyInfo.cs

--- SaveGameSyncer/Properties/AssemblyInfo.cs
+++ SaveGameSyncer/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SaveGameSyncer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("KSP Save Game Synchronisation Plugin")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("SaveGameSyncer")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright © James Lawrence 2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
